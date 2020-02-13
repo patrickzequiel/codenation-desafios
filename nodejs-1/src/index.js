2f683e615af6da901434420d5866347f7c542860
Raul Rory <raul.rory@somarmeteorologia.com.br>
2020-02-13 19:45:28 -0300

    Refatorando funções q4 e q5

--- nodejs-1/src/index.js
+++ nodejs-1/src/index.js
@@ -24,16 +24,24 @@ const q2 = () => {
 const q3 = () => planilha.slice(0, 20).map(nomes => nomes.full_name); 
 
 // Quem são os top 10 jogadores que ganham mais dinheiro (utilize as colunas `full_name` e `eur_wage`)?
-const q4 = () => {
-  planilha.sort((a,b) => a.eur_wage == b.eur_wage ? b.full_name - a.full_name : b.eur_wage - a.eur_wage)
+/* const q4 = () => {
+   planilha.sort((a,b) => a.eur_wage == b.eur_wage ? b.full_name - a.full_name : b.eur_wage - a.eur_wage)
   return planilha.slice(0, 10).map(lista => lista.planilha);
-}
+ */
+const q4 = () => planilha
+    .sort((a, b) => b.eur_wage - a.eur_wage || b.full_name < a.full_name )
+    .slice(0, 10)
+    .map(name => name.full_name)
 
 // Quem são os 10 jogadores mais velhos (use como critério de desempate o campo `eur_wage`)?
-const q5 = () => {
+/* const q5 = () => {
   planilha.sort((a, b) => a.age == b.age ? b.eur_wage - a.eur_wage : b.age - a.age)
   return planilha.slice(0, 10).map(lista => lista.planilha);
-}
+} */
+const q5 = () => planilha
+    .sort((a, b) => b.age - a.age || b.eur_wage - a.eur_wage )
+    .slice(0, 10)
+    .map(name => name.full_name)
 
 // Conte quantos jogadores existem por idade. Para isso, construa um mapa onde as chaves são as idades e os valores a contagem.
 const q6 = () => {
